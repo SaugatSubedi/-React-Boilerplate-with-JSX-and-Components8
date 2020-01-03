52954708d618b41b5bd39b355005ef3f4185b4b7
SaugatSubedi <subedisaugat176@gmail.com>
2020-01-03 15:05:11 +0545

    Readme created.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# -React-Boilerplate-with-JSX-and-Components8
+# React-Boilerplate-with-JSX-and-Components
+This is a # React-Boilerplate-with-JSX-and-Components.
+
+/*********Usage guideline*********/
+
+1. Clone the repository at your desired location git clone https://github.com/SaugatSubedi/-React-Boilerplate-with-JSX-and-Components8
+2. Use command "npm install" to download npm package.
+3. Use "npm run build" to start the project
+4. Go to browser. Enter URL "http://localhost:3000/"
+5. Application run sucessfully.
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const word = 'set';
-const element = <h1>This is the method to used {word} content.</h1>
+const word = 'third Assignment';
+const element = <h1>This is my  {word}.</h1>
 
 ReactDOM.render(element, document.getElementById('heading'));
 function Button(properties){
     return <button type="submit">{properties.label} </button>
 }
-ReactDOM.render(<Button label="save"/>, document.getElementById('btnSave'));
-ReactDOM.render(<Button label="Delete"/>, document.getElementById('btnDelete'));
-ReactDOM.render(<Button label="Reset"/>, document.getElementById('btnReset'));
+ReactDOM.render(<Button label="SAVE"/>, document.getElementById('save'));
+ReactDOM.render(<Button label="CANCEL"/>, document.getElementById('cancel'));
+ReactDOM.render(<Button label="RESET"/>, document.getElementById('reset'));
