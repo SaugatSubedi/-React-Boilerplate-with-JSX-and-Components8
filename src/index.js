fe3c010b111e114a9425b0deb160adda044ae41c
SaugatSubedi <subedisaugat176@gmail.com>
2020-01-03 13:02:54 +0545

    using function buttons created

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const word = 'set';
+const element = <h1>This is the method to used {word} content.</h1>
+
+ReactDOM.render(element, document.getElementById('heading'));
+function Button(properties){
+    return <button type="submit">{properties.label} </button>
+}
+ReactDOM.render(<Button label="save"/>, document.getElementById('btnSave'));
+ReactDOM.render(<Button label="Delete"/>, document.getElementById('btnDelete'));
+ReactDOM.render(<Button label="Reset"/>, document.getElementById('btnReset'));
